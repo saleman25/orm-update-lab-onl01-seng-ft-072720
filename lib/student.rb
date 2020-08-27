c3474add40f07039330f193f38ece28ad1940d2e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 17:32:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ end
 
  def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.name)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
