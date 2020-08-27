c336401d72388253f649989da1eace1158349d58
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 17:41:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ def self.drop_table
   DB[:conn].execute(sql)
 end
 
- def save
+def save
   if self.id
     self.update
   else
@@ -37,7 +37,7 @@ end
 
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
 end
     
@@ -60,17 +60,17 @@ def self.find_by_name(name)
     FROM students
     WHERE name = ?
     LIMIT 1
-    SQL
+  SQL
 
-    DB[:conn].execute(sql,name).map do |row|
+  DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
 end.first
 end
 
-  def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
+def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+end
 
 end
 
