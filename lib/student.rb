46eea58d1353cb57342ce7b8b43df1798ed2495f
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 17:17:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,8 +30,16 @@ def self.drop_table
 end
 
 def save 
-  
+  sql = <<-SQL 
+  INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
 
+    DB[:conn].execute(sql, self.name, self.grade)
+end 
+
+def self.create 
+  
 
 
 
