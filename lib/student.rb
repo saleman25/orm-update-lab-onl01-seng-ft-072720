1ba1b405aee9558267b5bffb2d44aa0c0d384488
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 17:37:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,11 +40,11 @@ end
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
     
+def self.create(name, grade)
+  student = Student.new(name, grade)
+  student.save
+end 
 
- def self.create(name, grade)
-    student = Student.new(name, grade)
-    student.save
-  end
 
 def self.new_from_db(row)
   id = row[0]
