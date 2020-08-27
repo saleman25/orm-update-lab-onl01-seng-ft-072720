831e196839c4a363428b439139b365426d1b19ac
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 17:20:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,13 @@ def self.create(name, grade)
   student
 end
 
-
+def self.new_from_db(row)
+  student = self.new
+  student.id = row[0]
+  student.name = row[1]
+  student.grade = row[2]
+  student
+end
 
 end
 
