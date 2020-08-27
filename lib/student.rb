4f8089865b7b8ebde38fa3b2d976744abab64326
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 17:21:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
 
-attr_accessor :name, :grade 
-attr_reader :id
+attr_accessor :name, :grade, :id
 
 def initialize(name, grade, id = NIL)
   @name = name 
